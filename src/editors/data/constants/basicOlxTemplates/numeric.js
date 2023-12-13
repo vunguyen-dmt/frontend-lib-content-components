b29c15ea2b959f7f4db9e9189a61aca7ecf5fe5c
Vu Nguyen <vu.nguyen@dmttek.com>
2023-12-14 06:37:06 +0700

    dist

--- src/editors/data/constants/basicOlxTemplates/numeric.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export const numeric = `<problem>
-  <numericalresponse>
-    <responseparam type="tolerance" default="5"/>
-    <formulaequationinput/>
-  </numericalresponse>
-</problem>`
-
-export default numeric;
